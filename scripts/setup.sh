f7e6123b7623d9db67b8bedccbb6d01e9d6d3bfb
Hackjack <jack03113@gmail.com>
2020-02-11 14:08:24 +0100

    Update modules

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,12 +36,19 @@ create_dir ${work_dir}/plugins
 pushd ${work_dir}/plugins
 #Extensions
 
-array=(geoserver-$GS_VERSION-vectortiles-plugin.zip geoserver-$GS_VERSION-css-plugin.zip \
-geoserver-$GS_VERSION-csw-plugin.zip geoserver-$GS_VERSION-wps-plugin.zip geoserver-$GS_VERSION-printing-plugin.zip \
-geoserver-$GS_VERSION-libjpeg-turbo-plugin.zip geoserver-$GS_VERSION-control-flow-plugin.zip \
-geoserver-$GS_VERSION-pyramid-plugin.zip geoserver-$GS_VERSION-gdal-plugin.zip \
+array=(geoserver-$GS_VERSION-vectortiles-plugin.zip \
+geoserver-$GS_VERSION-css-plugin.zip \
+geoserver-$GS_VERSION-csw-plugin.zip \
+geoserver-$GS_VERSION-wps-plugin.zip \
+geoserver-$GS_VERSION-printing-plugin.zip \
+geoserver-$GS_VERSION-libjpeg-turbo-plugin.zip \
+geoserver-$GS_VERSION-control-flow-plugin.zip \
+geoserver-$GS_VERSION-pyramid-plugin.zip \
+geoserver-$GS_VERSION-gdal-plugin.zip \
 geoserver-$GS_VERSION-sldservice-plugin.zip  \
-geoserver-$GS_VERSION-importer-plugin.zip geoserver-$GS_VERSION-charts-plugin.zip)
+geoserver-$GS_VERSION-importer-plugin.zip \
+geoserver-$GS_VERSION-charts-plugin.zip)
+
 for i in "${array[@]}"
 do
     url="https://sourceforge.net/projects/geoserver/files/GeoServer/${GS_VERSION}/extensions/${i}/download"
